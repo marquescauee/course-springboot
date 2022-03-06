e479d17c74fc9044d79be044e027bb0df091fd05
marquescauee <cauelopesmarques123@gmail.com>
2022-03-05 22:27:04 -0300

    Payment, one-to-one association

--- src/main/java/br/com/cauelopesmarques/course/config/TestConfig.java
+++ src/main/java/br/com/cauelopesmarques/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.cauelopesmarques.course.models.Category;
 import br.com.cauelopesmarques.course.models.Order;
 import br.com.cauelopesmarques.course.models.OrderItem;
+import br.com.cauelopesmarques.course.models.Payment;
 import br.com.cauelopesmarques.course.models.Product;
 import br.com.cauelopesmarques.course.models.User;
 import br.com.cauelopesmarques.course.models.enums.OrderStatus;
@@ -78,5 +79,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		oir.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		or.save(o1);
 	}
 }
